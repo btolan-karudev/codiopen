85f9e3f01e1cff15fb23dbaa4d9b73451b7c8358
Beniamin Tolan <beniamin.tolan@outlook.com>
2019-07-03 17:13:04 +0200

    constructeur

--- application/controllers/Forum.php
+++ application/controllers/Forum.php
@@ -2,25 +2,25 @@
 
 class Forum extends CI_Controller
 {
+    private $titre_defaut;
+
+    public function __construct()
+    {
+        //	Obligatoire
+        parent::__construct();
+
+        //	Maintenant, ce code sera exécuté chaque fois que ce contrôleur sera appelé.
+        $this->titre_defaut = 'Mon super site';
+        echo 'Bonjour !';
+    }
+
+    public function index()
+    {
+        $this->accueil();
+    }
+
     public function accueil()
     {
-        echo 'Hello World!';
+        var_dump($this->titre_defaut);
     }
-
-    //	Cette page accepte une variable $_GET facultative
-    public function bonjour($pseudo = '')
-    {
-        echo 'Salut à toi : ' . $pseudo;
-    }
-
-    //	Cette page accepte deux variables $_GET facultatives
-    public function manger($plat = '', $boisson = '')
-    {
-        echo 'Voici votre menu : <br />';
-        echo $plat . '<br />';
-        echo $boisson . '<br />';
-        echo 'Bon appétit !';
-    }
-
-
 }

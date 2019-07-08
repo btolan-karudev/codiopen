1342012b02cebf61ebaf795581946e68abe91ff0
Beniamin Tolan <beniamin.tolan@outlook.com>
2019-07-08 11:49:21 +0200

    helper url

--- application/views/news/vue.php
+++ application/views/news/vue.php
@@ -1,21 +1,16 @@
-<h1>Bonjour</h1>
+<h1>
+    Test
+</h1>
 
 <p>
-    Ceci est mon paragraphe !
-</p>
+    <a href="<?php echo site_url(); ?>">accueil</a>
+    <br />
 
-<p>
-    Votre pseudo est <?php echo $pseudo; ?>.
-</p>
+    <a href="<?php echo site_url('test'); ?>">accueil</a> du test
+    <br />
 
-<p>
-    Votre email est <?php echo $email; ?>.
-</p>
+    <a href="<?php echo site_url('test/secret'); ?>">page secrète</a>
+    <br />
 
-<p>
-    <?php if($en_ligne): ?>
-        Vous êtes en ligne.
-    <?php else: ?>
-        Vous n'êtes pas en ligne.
-    <?php endif; ?>
+    <a href="<?php echo site_url(array('test', 'secret')); ?>">page secrète</a>
 </p>
